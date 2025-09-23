3a8c2b132a6b354be38199d396f4aeccaf46bcbd
Brylle <bryanpalay119@gmail.com>
2025-09-23 17:34:54 +0800

    Frontend

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,178 @@
+"use client"
+
+import * as React from "react"
+import {
+  IconCamera,
+  
+  IconChartBar,
+  IconDashboard,
+  IconDatabase,
+  IconFileAi,
+  IconFileDescription,
+  IconFileWord,
+  IconFolder,
+  IconBook,
+  IconInnerShadowTop,
+  IconHome,
+  IconReport,
+  IconSearch,
+  IconSettings,
+  IconUsers,
+  IconBed,
+  IconCurrencyPeso,
+  IconDiamond,
+  IconReceipt
+} from "@tabler/icons-react"
+
+import { NavDocuments } from "@/components/nav-documents"
+import { NavMain } from "@/components/nav-main"
+import { NavSecondary } from "@/components/nav-secondary"
+import { NavUser } from "@/components/nav-user"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import { DollarSignIcon } from "lucide-react"
+
+const data = {
+  user: {
+    name: "Amethyst Inn Admin",
+    email: "amethystinnadmin@gmail.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "#",
+      icon: IconDashboard,
+    },
+    {
+      title: "Room Management",
+      url: "#",
+      icon: IconHome,
+    },
+    {
+      title: "Booking Management",
+      url: "#",
+      icon: IconBook,
+    },
+    {
+      title: "Payment Management",
+      url: "#",
+      icon: IconCurrencyPeso,
+    },
+    {
+      title: "Guest Management",
+      url: "#",
+      icon: IconUsers,  
+    },
+    {
+      title: "User Management",
+      url: "#",
+      icon: IconUsers,
+    },
+  ],
+  navClouds: [
+    {
+      title: "Capture",
+      icon: IconCamera,
+      isActive: true,
+      url: "#",
+      items: [
+        {
+          title: "Active Proposals",
+          url: "#",
+        },
+        {
+          title: "Archived",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Proposal",
+      icon: IconFileDescription,
+      url: "#",
+      items: [
+        {
+          title: "Active Proposals",
+          url: "#",
+        },
+        {
+          title: "Archived",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Prompts",
+      icon: IconFileAi,
+      url: "#",
+      items: [
+        {
+          title: "Active Proposals",
+          url: "#",
+        },
+        {
+          title: "Archived",
+          url: "#",
+        },
+      ],
+    },
+  ],
+  navSecondary: [
+    {
+      title: "Settings",
+      url: "#",
+      icon: IconSettings,
+    },
+    
+  ],
+  documents: [
+    {
+      name: "Payments Receipt",
+      url: "#",
+      icon: IconReceipt,
+    },
+    {
+      name: "Reports",
+      url: "#",
+      icon: IconReport,
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
+            >
+              <a href="#">
+                <IconDiamond className="!size-5" />
+                <span className="text-base font-semibold">Amethyst Inn</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        <NavDocuments items={data.documents} />
+        <NavSecondary items={data.navSecondary} className="mt-auto" />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
